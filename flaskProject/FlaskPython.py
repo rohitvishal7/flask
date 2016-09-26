fa9f24e4b7f4037bec10f6ed7b4488a2e2befa18
mod2695 <mail.mohit04@gmail.com>
2016-09-26 17:45:24 +0530

    Add files via upload

---
+++ flaskProject/FlaskPython.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,request,redirect,url_for
+app = Flask(__name__)
+news = {"sports":"sachin retired","politics":"Modi sarkaar","academics":"Harvard Hiring"}
+
+@app.route('/login',methods = ['POST'])
+def login():
+   if request.method == 'POST':
+      user = request.form['username']
+      if user=='mohit':
+         return render_template('user.html',news=news )
+      else:
+         return "invalid Credentials"
+
+@app.route('/login_redirect',methods = ['POST'])
+def login_redirect():
+   if request.method == 'POST':
+         return render_template('.html')
+      else:
+         return "invalid Credentials"
+
+
+@app.route("/")
+def index():
+   return render_template("home.html")
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+def hello_name(user):
+    return render_template('hello.html', name=user)
